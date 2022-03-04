a3f3a5f0dbda445805db0a73165a631782c03027
vjony <vjony@users.noreply.github.com>
2022-03-04 10:17:46 +0100

    summary

--- pages/destination.vue
+++ pages/destination.vue
@@ -1,3 +1,32 @@
 <template>
-  <h1>Destination</h1>
+  <h1 class="numbered-title"><span>01</span>Pick your destination</h1>
 </template>
+
+<script>
+export default {
+  data(){
+    return{
+      destinations: [],
+    }
+  },
+  methods: {
+    async loadData() {
+      const response = await fetch(
+        'https://space.wp.seiwald.hak-stjo.schulwebspace.at/wp-json/wp/v2/destination'
+      )
+      this.destination = await response.json()
+    },
+  },
+    mounted() {
+    this.loadData()
+    document.body.style.backgroundImage = "url('background-destination-mobile.jpg')"
+  },
+  }
+
+</script>
+
+<style scoped>
+.numbered-title{
+  font-size: 16px;
+}
+</style>
